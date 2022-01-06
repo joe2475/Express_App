4d5f3bf309560db4ef160190fef8d04fbb4a339f
joe2475 <33675219+joe2475@users.noreply.github.com>
2022-01-06 15:02:27 -0600

    updates

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
+const { response } = require("express");
 const express = require("express");
 const app = express(); 
+const port = 8080;
 
+//middleware imports
+const logger = require('./middleware/logger');
+const notFound = require('./middleware/notFound');
+const errorHandeler = require('./middleware/errorHandler');
+const errorHandler = require("./middleware/errorHandler");
+
+// middleware wire up
+app.use(logger);
+
+
+app.get('/', (req, res) =>
+{
+    res.send("It works!");
+})
+
+app.get('/something', (req, res) =>{
+    res.send("Something works");
+});
+
+
+app.get('/error', (req, res, next) => {
+   const mockError = {
+       status: 403,
+       message: 'Something went wrong'
+   }
+   next(mockError);
+})
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => {
+    console.log(`Now listening on port: ${port}`);
+});
---
+++ middleware/logger.js
@@ -0,0 +1,10 @@
+//logger middleware
+const logger = (req, res, next) => {
+    const {url, method} = req; 
+    const time = new Date();
+    const message = `${method}: ${url} - ${time}`;
+    console.log(message);
+    next();
+};
+
+module.exports = logger;
